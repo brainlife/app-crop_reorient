89dabbda45a802480a4c4d9dedc2c599114160a1
Soichi Hayashi <hayashis@iu.edu>
2018-10-12 14:23:37 -0400

    updated to use singularit container

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -f out.nii.gz
+
+input=$(jq -r .input config.json)
+
+cp $input out.nii.gz
+
+if [ $(jq -r .reorient config.json) == true ]; then
+	fslreorient2std out.nii.gz out.nii.gz
+fi
+
+if [ $(jq -r .crop config.json) == true ]; then
+	robustfov -i out.nii.gz -r out.nii.gz
+fi
+
+
