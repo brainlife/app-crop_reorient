e54571159a6019fe1f8a9f26860a5b0bc2fa0387
Soichi Hayashi <soichih@gmail.com>
2021-04-09 12:42:11 -0400

    updated so that it can handle any format that fslreorient2std and robustfov can handle

--- run.sh
+++ run.sh
@@ -8,18 +8,14 @@ input=$(jq -r .input config.json)
 if [ $(jq -r .reorient config.json) == true ]; then
     fslreorient2std $input tmp1.nii.gz
 else
-    ln -s $input tmp1.nii.gz
+    ln -sf $input tmp1.nii.gz
 fi
 
 if [ $(jq -r .crop config.json) == true ]; then
     robustfov -i tmp1.nii.gz -r tmp2.nii.gz
 else
-    ln -s tmp1.nii.gz tmp2.nii.gz
+    ln -sf tmp1.nii.gz tmp2.nii.gz
 fi
 
-[ $(basename $input) == "t1.nii.gz" ] && outname=out/t1.nii.gz
-[ $(basename $input) == "t2.nii.gz" ] && outname=out/t2.nii.gz
-[ $(basename $input) == "bold.nii.gz" ] && outname=out/bold.nii.gz
-
 mkdir -p out
-ln -s ../tmp2.nii.gz $outname
+ln -sf ../tmp2.nii.gz out/$(basename $input)
