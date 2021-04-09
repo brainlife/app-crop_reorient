fbdf1769259617826d90a15513171809e3b49194
Soichi Hayashi <soichih@gmail.com>
2021-04-09 12:40:07 -0400

    updated to handle bold image as well as t1/t2

--- run.sh
+++ run.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-rm -f out.nii.gz
+set -e
+set -x
 
 input=$(jq -r .input config.json)
 
-cp $input out.nii.gz
-chmod +w out.nii.gz
-
 if [ $(jq -r .reorient config.json) == true ]; then
-	fslreorient2std out.nii.gz out.nii.gz
+    fslreorient2std $input tmp1.nii.gz
+else
+    ln -s $input tmp1.nii.gz
 fi
 
 if [ $(jq -r .crop config.json) == true ]; then
-	robustfov -i out.nii.gz -r out.nii.gz
+    robustfov -i tmp1.nii.gz -r tmp2.nii.gz
+else
+    ln -s tmp1.nii.gz tmp2.nii.gz
 fi
 
+[ $(basename $input) == "t1.nii.gz" ] && outname=out/t1.nii.gz
+[ $(basename $input) == "t2.nii.gz" ] && outname=out/t2.nii.gz
+[ $(basename $input) == "bold.nii.gz" ] && outname=out/bold.nii.gz
 
+mkdir -p out
+ln -s ../tmp2.nii.gz $outname
